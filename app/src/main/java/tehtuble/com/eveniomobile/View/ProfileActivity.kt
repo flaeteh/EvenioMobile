acfa9b4c95e23da18587c0c08213481fdf907200
flaeteh <flaeteh@gmail.com>
2018-12-01 04:40:04 +0800

    Updated UI

--- app/src/main/java/tehtuble/com/eveniomobile/View/ProfileActivity.kt
+++ app/src/main/java/tehtuble/com/eveniomobile/View/ProfileActivity.kt
@@ -9,6 +9,9 @@ import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_profile.*
 import tehtuble.com.eveniomobile.View.Adapter.ProfilePageAdapter
 import tehtuble.com.eveniomobile.R
+import android.view.ViewGroup
+
+
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -22,12 +25,23 @@ class ProfileActivity : AppCompatActivity() {
         var attendedEventsFragment = pageAdapter.getAttendedEventsFragment()
         var pendingEventsFragment = pageAdapter.getPendingEventsFragment()
 
+
         viewPager.adapter = pageAdapter
         viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout as TabLayout?))
 
         img_home.setOnClickListener {
             onClickHome()
         }
+
+        tabLayout?.setTabTextColors(resources.getColor(R.color.lightGray), resources.getColor(R.color.White))
+        for (i in 0 until tabLayout?.tabCount!!) {
+            val tab = (tabLayout?.getChildAt(0) as ViewGroup).getChildAt(i)
+            val p = tab.layoutParams as ViewGroup.MarginLayoutParams
+            p.setMargins(20, 0, 20, 0)
+            tab.requestLayout()
+        }
+
+
     }
 
     fun onClickHome() {
